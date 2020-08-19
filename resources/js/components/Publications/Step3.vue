a2479a5358dc7b20a93ff3852348f3889a9db993
Andrey <gorand.one@gmail.com>
2020-08-19 19:41:39 +0300

    step 3

--- resources/js/components/Publications/Step3.vue
+++ resources/js/components/Publications/Step3.vue
@@ -1,51 +1,88 @@
 <template>
     <div>
         <p class="step-subtitle">
-            Крок 3 з 4. Назва та база для публікації
+            Крок 3 з 4.
         </p>
 
         <div class=" step-content">
-            <div class=" step-item ">
-                <p class="item-title">Коефіцієнт впливовості SNIP журналу SCOPUS :</p>
-                <input type="number" class="item-input" v-model="stepData.snip">
-            </div>
-            <div class=" step-item">
-                <p class="item-title">Коефіцієнт впливовості імпакт-фактор журналу WoS :</p>
-                <input type="text" class="item-input" v-model="stepData.impact_factor">
-            </div>
-            <div class="step-item">
-                <p class="item-title">Квартиль журналу БД Scopus :</p>
-                <div class="categories-elem" v-for="n in 4" :key="n">
-                    <input :id="'scopus' + n" type="radio" v-model="stepData.quartil_scopus" :value="n">
-                    <label class="small-box" :for="'scopus' + n">{{n}}</label>
+            <form class="form-block">
+
+                <div class="form-row">
+                    <div class="form-group col-lg-8">
+                        <label >SNIP журналу (БД Scopus)</label>
+                        <div class="input-container">
+                            <input type="text" v-model="stepData.snip">
+                            <div class="hint" ><span>Прізвище, ім’я, по-батькові:</span></div>
+                        </div>
+                    </div>
+                    <div class="form-group col-lg-4">
+                        <label >Квартиль журналу (БД Scopus)</label>
+                        <div class="input-container">
+
+                            <select  v-model="stepData.quartil_scopus" >
+                                <option value=""></option>
+                                <option v-for="n in 4" :key="n" :value="n">{{n }}</option>
+
+
+                            </select>
+                            <div class="hint" ><span>Прізвище, ім’я, по-батькові:</span></div>
+                        </div>
+                    </div>
+
                 </div>
-            </div>
-            <div class="step-item">
-                <p class="item-title">Квартиль журналу БД WoS :</p>
-                <div class="categories-elem" v-for="n in 4" :key="n">
-                    <input :id="'wos' + n" type="radio" v-model="stepData.quartil_wos" :value="n">
-                    <label class="small-box" :for="'wos' + n">{{n}}</label>
+
+                <div class="form-row">
+                    <div class="form-group col-lg-8">
+                        <label >Імпакт-фактор (БД WoS)</label>
+                        <div class="input-container">
+                            <input type="text" v-model="stepData.impact_factor">
+                            <div class="hint" ><span>Прізвище, ім’я, по-батькові:</span></div>
+                        </div>
+                    </div>
+                    <div class="form-group col-lg-4">
+                        <label >Квартиль журналу (БД WoS)</label>
+                        <div class="input-container">
+
+                            <select  v-model="stepData.quartil_wos" >
+                                <option value=""></option>
+                                <option v-for="n in 4" :key="n" :value="n">{{n }}</option>
+
+
+                            </select>
+                            <div class="hint" ><span>Прізвище, ім’я, по-батькові:</span></div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class=" step-item">
-                <p class="item-title">Підбаза WoS :</p>
-                <input id="subDb0" type="radio" class="d-none" v-model="stepData.sub_db_index" value="0" >
-                <div class="categories-elem" >
-                    <input id="subDb1" type="checkbox" v-model="subWos1"  >
-                    <label class="big-box" for="subDb1" @click="changeType(1)">Science Citation Index Expanded (SCIE) </label>
+
+                <div class="form-row">
+                    <div class="form-group col-lg-8">
+                        <label >Підбаза WoS</label>
+                        <div class="input-container">
+                            <select  v-model="stepData.sub_db_index" >
+                                <option value="0"></option>
+                                <option value="1">Science Citation Index Expanded (SCIE)</option>
+                                <option value="2">Social Science Citation Index</option>
+
+
+                            </select>
+                            <div class="hint" ><span>Прізвище, ім’я, по-батькові:</span></div>
+                        </div>
+                    </div>
+
+
                 </div>
-                <div class="categories-elem" >
-                    <input id="subDb2" type="checkbox" v-model="subWos2"  >
-                    <label class="big-box" for="subDb2" @click="changeType(2)">Social Science Citation Index</label>
-                </div>
-            </div>
+
+
+
+
+            </form>
 
 
 
         </div>
 
         <div class="step-button-group">
-            <button class="next active" @click="nextStep">Продовжити <span>&gt;</span></button>
+            <button class="next active" @click="nextStep">Продовжити </button>
             <button class="prev" @click="prevStep">На попередній крок</button>
         </div>
     </div>
@@ -56,8 +93,7 @@
         name: "Step4",
         data() {
             return {
-                subWos1: false,
-                subWos2: false,
+
 
                 stepData:{
                     snip: '',
@@ -71,23 +107,23 @@
         },
         methods:{
             nextStep() {
-                this.stepData.sub_db_index = this.subWos1 ? 1 : this.subWos2 ? 2 : 0;
+
                 this.$emit('getData', this.stepData);
             },
             prevStep(){
                 this.$emit('prevStep');
             },
-            changeType(type){
-                if(type == 1 && this.subWos1 == false && this.subWos2 == true){
-                    this.subWos2 = false;
-                }
-                else if(type == 2 && this.subWos1 == true && this.subWos2 == false){
-                    this.subWos1 = false;
-                }
-
-
-
-            },
+            // changeType(type){
+            //     if(type == 1 && this.subWos1 == false && this.subWos2 == true){
+            //         this.subWos2 = false;
+            //     }
+            //     else if(type == 2 && this.subWos1 == true && this.subWos2 == false){
+            //         this.subWos1 = false;
+            //     }
+            //
+            //
+            //
+            // },
 
 
         },
@@ -100,80 +136,80 @@
 <style lang="scss" scoped>
 
 
-    .step-item{
-        margin-top: 50px;
-        display: flex;
-        align-items: center;
-        &:first-of-type{
-            margin-top: 0;
-        }
-        .item-title{
-            width: auto;
-            font-family: Montserrat;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 25px;
-            color: #A6A6A6;
-            margin-right: 20px;
-            margin-bottom: 0;
-        }
-        .item-input{
-            padding: 12px;
-            text-align: center;
-            box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.25);
-            border-radius: 44.5px;
-            border: none;
-            outline: none;
-            font-family: Montserrat;
-            font-style: normal;
-            font-weight: normal;
-            font-size: 20px;
-            color: #0E0E0E;
-            min-width: 258px;
+    /*.step-item{*/
+    /*    margin-top: 50px;*/
+    /*    display: flex;*/
+    /*    align-items: center;*/
+    /*    &:first-of-type{*/
+    /*        margin-top: 0;*/
+    /*    }*/
+    /*    .item-title{*/
+    /*        width: auto;*/
+    /*        font-family: Montserrat;*/
+    /*        font-style: normal;*/
+    /*        font-weight: normal;*/
+    /*        font-size: 25px;*/
+    /*        color: #A6A6A6;*/
+    /*        margin-right: 20px;*/
+    /*        margin-bottom: 0;*/
+    /*    }*/
+    /*    .item-input{*/
+    /*        padding: 12px;*/
+    /*        text-align: center;*/
+    /*        box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.25);*/
+    /*        border-radius: 44.5px;*/
+    /*        border: none;*/
+    /*        outline: none;*/
+    /*        font-family: Montserrat;*/
+    /*        font-style: normal;*/
+    /*        font-weight: normal;*/
+    /*        font-size: 20px;*/
+    /*        color: #0E0E0E;*/
+    /*        min-width: 258px;*/
 
-        }
-        .categories-elem{
-            display: flex;
+    /*    }*/
+    /*    .categories-elem{*/
+    /*        display: flex;*/
 
-            .small-box{
-                width: 31px;
-                padding: 4px 0;
-                margin: 0 10px;
-                border: 1px solid #18A0FB;
-                box-sizing: border-box;
-                border-radius: 10px;
-                font-family: Montserrat;
-                font-style: normal;
-                font-weight: normal;
-                font-size: 20px;
-                color: #18A0FB;
-                text-align: center;
-                cursor: pointer;
-            }
-            .big-box{
-                padding: 10px 40px;
-                margin: 0 10px;
-                border: 1px solid #18A0FB;
-                box-sizing: border-box;
-                border-radius: 44.5px;
-                font-family: Montserrat;
-                font-style: normal;
-                font-weight: normal;
-                font-size: 20px;
-                color: #18A0FB;
-                cursor: pointer;
+    /*        .small-box{*/
+    /*            width: 31px;*/
+    /*            padding: 4px 0;*/
+    /*            margin: 0 10px;*/
+    /*            border: 1px solid #18A0FB;*/
+    /*            box-sizing: border-box;*/
+    /*            border-radius: 10px;*/
+    /*            font-family: Montserrat;*/
+    /*            font-style: normal;*/
+    /*            font-weight: normal;*/
+    /*            font-size: 20px;*/
+    /*            color: #18A0FB;*/
+    /*            text-align: center;*/
+    /*            cursor: pointer;*/
+    /*        }*/
+    /*        .big-box{*/
+    /*            padding: 10px 40px;*/
+    /*            margin: 0 10px;*/
+    /*            border: 1px solid #18A0FB;*/
+    /*            box-sizing: border-box;*/
+    /*            border-radius: 44.5px;*/
+    /*            font-family: Montserrat;*/
+    /*            font-style: normal;*/
+    /*            font-weight: normal;*/
+    /*            font-size: 20px;*/
+    /*            color: #18A0FB;*/
+    /*            cursor: pointer;*/
 
-            }
-            input{
-                display: none;
-            }
-            input:checked +label{
-                background: #18A0FB;
-                border: 1px solid #18A0FB;
-                color: #fff;
-            }
-        }
-    }
+    /*        }*/
+    /*        input{*/
+    /*            display: none;*/
+    /*        }*/
+    /*        input:checked +label{*/
+    /*            background: #18A0FB;*/
+    /*            border: 1px solid #18A0FB;*/
+    /*            color: #fff;*/
+    /*        }*/
+    /*    }*/
+    /*}*/
 
 
 </style>
