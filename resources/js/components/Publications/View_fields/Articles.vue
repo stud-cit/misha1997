307afd96e1e81e0a2f14ff9df5dbf6b75871ba71
tenda150 <alexeyprikazchik@gmail.com>
2020-09-01 15:14:23 +0300

    new fields to views

--- resources/js/components/Publications/View_fields/Articles.vue
+++ resources/js/components/Publications/View_fields/Articles.vue
@@ -9,6 +9,71 @@
             <div class="col-lg-9 list-item list-text">{{data.initials}}</div>
         </li>
         <li class="row">
+            <div class="col-lg-3 list-item list-title">Вид публікації:</div>
+            <div class="col-lg-9 list-item list-text">{{data.publication_type.title}}</div>
+        </li>
+        <template v-if="data.supervisor_id!=null">
+            <li class="row">
+                <div class="col-lg-3 list-item list-title">Під керівництвом:</div>
+                <div class="col-lg-9 list-item list-text">Так</div>
+            </li>
+            <li class="row">
+                <div class="col-lg-3 list-item list-title">Керівник:</div>
+                <div class="col-lg-9 list-item list-text">{{data.supervisor.name}}</div>
+            </li>
+        </template>
+        
+        <template v-else>
+            <li class="row">
+                <div class="col-lg-3 list-item list-title">Під керівництвом:</div>
+                <div class="col-lg-9 list-item list-text">Ні</div>
+            </li>
+        </template>
+
+        
+
+        <template v-if="data.science_type_id!=null">
+            <li class="row">
+                <div class="col-lg-3 list-item list-title">БД Scopus\WoS:</div>
+                <div class="col-lg-9 list-item list-text">{{data.science_type.type}}</div>
+            </li>
+
+            <template v-if="data.science_type_id!=2">
+                <li class="row">
+                    <div class="col-lg-3 list-item list-title" v-if="data.science_type_id==1">SNIP журналу:</div>
+                    <div class="col-lg-3 list-item list-title" v-if="data.science_type_id==3">SNIP журналу (БД Scopus):</div>
+                    <div class="col-lg-9 list-item list-text">{{data.snip}}</div>
+                </li>
+                <li class="row">
+                    <div class="col-lg-3 list-item list-title" v-if="data.science_type_id==1">Квартиль журналу:</div>
+                    <div class="col-lg-3 list-item list-title" v-if="data.science_type_id==3">Квартиль журналу (БД Scopus):</div>
+                    <div class="col-lg-9 list-item list-text">{{data.quartil_scopus}}</div>
+                </li>
+            </template>
+
+            <template v-if="data.science_type_id!=1">
+                <li class="row">
+                    <div class="col-lg-3 list-item list-title" v-if="data.science_type_id==2">Імпакт-фактор:</div>
+                    <div class="col-lg-3 list-item list-title" v-if="data.science_type_id==3">Імпакт-фактор (БД WoS):</div>
+                    <div class="col-lg-9 list-item list-text">{{data.impact_factor}}</div>
+                </li>
+                <li class="row">
+                    <div class="col-lg-3 list-item list-title" v-if="data.science_type_id==2">Квартиль журналу:</div>
+                    <div class="col-lg-3 list-item list-title" v-if="data.science_type_id==3">Квартиль журналу (БД WoS):</div>
+                    <div class="col-lg-9 list-item list-text">{{data.quartil_wos}}</div>
+                </li>
+
+                <li class="row">
+                    <div class="col-lg-3 list-item list-title">Підбаза WoS:</div>
+                    <div class="col-lg-9 list-item list-text" v-if="data.sub_db_index==1">Science Citation Index Expanded (SCIE)</div>
+                    <div class="col-lg-9 list-item list-text" v-if="data.sub_db_index==2">Social Science Citation Index</div>
+                </li>
+            </template>
+
+        </template>
+
+
+        <li class="row">
             <div class="col-lg-3 list-item list-title">Назва журналу:</div>
             <div class="col-lg-9 list-item list-text">{{data.name_magazine}}</div>
         </li>
