50efab8ad8c00d5e61288c55daedc76a9e90c0bd
misha1997 <mishaotroshenko2013@gmail.com>
2020-12-09 14:11:19 +0200

    fix

--- resources/js/components/Buttons/Back.vue
+++ resources/js/components/Buttons/Back.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <button @click="$router.go(-1)" class="next">Назад</button>
+        <button @click="$router.back()" class="next">Назад</button>
     </div>
 </template>
